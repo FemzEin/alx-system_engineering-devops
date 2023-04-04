fcc389edfaa3cf6217a26e68a658127d509a5b2d
Albert James <femzein@gmail.com>
2023-04-03 17:47:29 -0700

    Puppet update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,9 +3,10 @@ class nginx {
   package { 'nginx':
     ensure => installed,
   }
+  $encoded_hostname = URI.encode_www_form_component($facts['networking']['hostname'])
   file { '/etc/nginx/conf.d/custom_headers.conf':
     ensure  => file,
-    content => "add_header X-Served-By ${facts['networking']['hostname']};",
+    content => "add_header X-Served-By ${encoded_hostname};",
     notify  => Service['nginx'],
   }
   service { 'nginx':
