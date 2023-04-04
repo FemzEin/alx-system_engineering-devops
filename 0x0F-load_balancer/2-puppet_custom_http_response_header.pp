b23b95b199257f99bf2b16599b0ba43d67e281a4
Albert James <femzein@gmail.com>
2023-04-03 17:04:45 -0700

    Load Balancer advanced

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Install Nginx and configure custom HTTP header response
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+  file { '/etc/nginx/conf.d/custom_headers.conf':
+    ensure => file,
+    content => "add_header X-Served-By ${::hostname};",
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
+
+# Apply the nginx class to the node
+node 'default' {
+  class { 'nginx': }
+}
